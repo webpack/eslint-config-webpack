52b5e60949de0b1085d00ccbc72874d07e837261
alexander-akait <sheo13666q@gmail.com>
2025-06-11 01:12:36 +0300

    fix: relax `jest/prefer-lowercase-title`

--- configs/jest.js
+++ configs/jest.js
@@ -140,7 +140,7 @@ const recommendedConfig = {
 		"jest/prefer-jest-mocked": "error",
 
 		// Allow to use `MyClass` title in describe for class testing
-		"jest/prefer-lowercase-title": ["error", { "ignore": ["describe"] }],
+		"jest/prefer-lowercase-title": ["error", { ignore: ["describe"] }],
 
 		"jest/prefer-mock-promise-shorthand": "error",
 
