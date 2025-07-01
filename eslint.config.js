4745549aa7d706146b57d2357b69da5930d439ee
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2025-07-01 14:53:47 +0300

    fix: use node.js recommended rules by default (#63)

--- eslint.config.js
+++ eslint.config.js
@@ -15,13 +15,28 @@ export default defineConfig([
 	{
 		files: ["./validation/commonjs-package/**/*"],
 		extends: [configs["recommended-commonjs"]],
+		rules: {
+			"n/no-unpublished-require": "off",
+		},
 	},
 	{
 		files: ["./validation/module-package/**/*"],
 		extends: [configs["recommended-module"]],
+		rules: {
+			"n/no-unpublished-require": "off",
+		},
 	},
 	{
 		files: ["./validation/dirty-package/**/*"],
 		extends: [configs["recommended-dirty"]],
+		rules: {
+			"n/no-unpublished-require": "off",
+		},
+	},
+	{
+		files: ["./validation/hashbang.js"],
+		rules: {
+			"n/hashbang": "off",
+		},
 	},
 ]);
