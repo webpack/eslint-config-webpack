cf036a4eb092f7354f8325a9d93f2668c74219b9
alexander-akait <sheo13666q@gmail.com>
2025-07-14 18:58:35 +0300

    fix: disable `@typescript-eslint/triple-slash-reference` for markdown

--- configs/markdown.js
+++ configs/markdown.js
@@ -76,7 +76,7 @@ async function getMarkdownRecommendedConfig() {
 
 				"@typescript-eslint/no-unused-vars": "off",
 
-				"@typescript-eslint/triple-slash-reference": "off"
+				"@typescript-eslint/triple-slash-reference": "off",
 			},
 		},
 	];
