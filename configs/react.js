44a2d9096f0276ffff8561a2dc5549094d4dfec1
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2025-07-14 15:21:54 +0300

    fix: loading react plugin

--- configs/react.js
+++ configs/react.js
@@ -1,26 +1,42 @@
-import reactPlugin from "eslint-plugin-react";
+/**
+ * @returns {Promise<Record<string, string>>} config
+ */
+async function getReactRecommendedConfig() {
+	let reactPlugin;
 
-const { recommended, "jsx-runtime": jsxRuntime } = reactPlugin.configs.flat;
+	try {
+		reactPlugin = (await import("eslint-plugin-react")).default;
+		// eslint-disable-next-line unicorn/prefer-optional-catch-binding
+	} catch (_err) {
+		// Nothing
+	}
 
-const recommendedReactConfig = {
-	...reactPlugin.configs.flat.recommended,
-	files: ["**/*.{jsx,tsx}"],
-	settings: {
-		react: {
-			version: "detect",
-			defaultVersion: "19",
+	const { recommended, "jsx-runtime": jsxRuntime } = (reactPlugin &&
+		reactPlugin.configs &&
+		reactPlugin.configs.flat) || { recommended: {}, "jsx-runtime": {} };
+
+	return {
+		...recommended,
+		files: ["**/*.{jsx,tsx}"],
+		settings: {
+			react: {
+				version: "detect",
+				defaultVersion: "19",
+			},
 		},
-	},
-	languageOptions: {
-		...recommended.languageOptions,
-		...jsxRuntime.languageOptions,
-	},
-	rules: {
-		...recommended.rules,
-		...jsxRuntime.rules,
-	},
-};
+		languageOptions: {
+			...recommended.languageOptions,
+			...jsxRuntime.languageOptions,
+		},
+		rules: {
+			...recommended.rules,
+			...jsxRuntime.rules,
+		},
+	};
+}
+
+const reactRecommendedConfig = await getReactRecommendedConfig();
 
 export default {
-	"react/recommended": recommendedReactConfig,
+	"react/recommended": reactRecommendedConfig,
 };
