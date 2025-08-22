ecf8020b751c89cf175fae0eb3d0307123e06db5
alexander-akait <sheo13666q@gmail.com>
2025-08-22 16:10:23 +0300

    fix: do not apply typescript presets without typescript

--- configs.js
+++ configs.js
@@ -1,129 +1,10 @@
-import fs from "node:fs";
-import path from "node:path";
 import { globalIgnores } from "eslint/config";
 import semver from "semver";
 import configs from "./configs/index.js";
 import { typescriptExtensions } from "./configs/utils/extensions.js";
+import getJsonFile from "./configs/utils/get-json-file.js";
+import isTypescriptInstalled from "./configs/utils/is-typescript-installed.js";
 import ignorePaths from "./ignore-paths.js";
-
-const SKIP_TIME = 5000;
-
-class Cache {
-	/**
-	 * Initialize this cache instance.
-	 */
-	constructor() {
-		this.map = new Map();
-	}
-
-	/**
-	 * Get the cached value of the given key.
-	 * @param {string} key The key to get.
-	 * @returns {import('type-fest').JsonObject} The cached value or null.
-	 */
-	get(key) {
-		const entry = this.map.get(key);
-		const now = Date.now();
-
-		if (entry) {
-			if (entry.expire > now) {
-				entry.expire = now + SKIP_TIME;
-				return entry.value;
-			}
-			this.map.delete(key);
-		}
-		return null;
-	}
-
-	/**
-	 * Set the value of the given key.
-	 * @param {string} key The key to set.
-	 * @param {import('type-fest').JsonObject} value The value to set.
-	 * @returns {void}
-	 */
-	set(key, value) {
-		const entry = this.map.get(key);
-		const expire = Date.now() + SKIP_TIME;
-
-		if (entry) {
-			entry.value = value;
-			entry.expire = expire;
-		} else {
-			this.map.set(key, { value, expire });
-		}
-	}
-}
-
-const cache = new Cache();
-
-/**
- * Reads the `package.json` data in a given path.
- *
- * Don't cache the data.
- * @param {string} dir The path to a directory to read.
- * @param {string} filename The filename.
- * @returns {import('type-fest').JsonObject|null} The read `package.json` data, or null.
- */
-function readJsonFile(dir, filename) {
-	const filePath = path.join(dir, filename);
-	try {
-		const text = fs.readFileSync(filePath, "utf8");
-		const data = JSON.parse(text);
-
-		if (
-			data !== null &&
-			typeof data === "object" &&
-			Array.isArray(data) === false
-		) {
-			data.filePath = filePath;
-			return data;
-		}
-		// eslint-disable-next-line unicorn/prefer-optional-catch-binding
-	} catch (_err) {
-		// do nothing.
-	}
-
-	return null;
-}
-
-/**
- * Gets a `package.json` data.
- * The data is cached if found, then it's used after.
- * @param {string} filename The filename.
- * @param {string=} startPath A file path to lookup.
- * @returns {import('type-fest').JsonObject | null} A found `package.json` data or `null`.
- * This object have additional property `filePath`.
- */
-function getJsonFile(filename, startPath = "a.js") {
-	const startDir = path.dirname(path.resolve(startPath));
-	let dir = startDir;
-	let prevDir = "";
-	let data = null;
-
-	do {
-		data = cache.get(dir + filename);
-		if (data) {
-			if (dir !== startDir) {
-				cache.set(startDir + filename, data);
-			}
-			return data;
-		}
-
-		data = readJsonFile(dir, filename);
-		if (data) {
-			cache.set(dir + filename, data);
-			cache.set(startDir + filename, data);
-			return data;
-		}
-
-		// Go to next.
-		prevDir = dir;
-		dir = path.resolve(dir, "..");
-	} while (dir !== prevDir);
-
-	cache.set(startDir + filename, null);
-	return null;
-}
 
 const packageJson = getJsonFile("package.json");
 const isModule =
@@ -199,28 +80,18 @@ function getJavascriptConfig() {
  * @returns {Promise<Record<string, string>>} config
  */
 function getTypescriptJSdocConfig() {
-	if (packageJson === null) {
-		return [];
-	}
-
-	const dependencies = packageJson.dependencies || [];
-	const devDependencies = packageJson.devDependencies || [];
-
-	return typeof dependencies.typescript !== "undefined" ||
-		typeof devDependencies.typescript !== "undefined"
-		? configs["typescript/jsdoc"]
-		: [];
+	return isTypescriptInstalled() ? configs["typescript/jsdoc"] : [];
 }
 
 /**
  * @returns {Promise<Record<string, string>>} config
  */
 function getTypescriptConfig() {
-	const tsconfigJson = getJsonFile("tsconfig.json");
-
-	if (!tsconfigJson) {
+	if (!isTypescriptInstalled()) {
 		return {};
 	}
+
+	const tsconfigJson = getJsonFile("tsconfig.json");
 
 	const isNoEmitEnabled =
 		(tsconfigJson &&
---
+++ configs/utils/is-typescript-installed.js
@@ -0,0 +1,22 @@
+import getJsonFile from "./get-json-file.js";
+
+/**
+ * @returns {boolean} true when typescript is supported by project, otherwise false
+ */
+function isTypescriptInstalled() {
+	const packageJson = getJsonFile("package.json");
+
+	if (packageJson === null) {
+		return [];
+	}
+
+	const dependencies = packageJson.dependencies || [];
+	const devDependencies = packageJson.devDependencies || [];
+
+	return Boolean(
+		typeof dependencies.typescript !== "undefined" ||
+			typeof devDependencies.typescript !== "undefined",
+	);
+}
+
+export default isTypescriptInstalled;
